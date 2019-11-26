24f02eb544f9e3eb276501d54ba1dc1663ef239a
elena-117 <3effir@gmail.com>
2019-11-26 16:28:46 +0300

    Дом. задание 4

--- Home_work_4/task_3/js/Module.js
+++ Home_work_4/task_3/js/Module.js
@@ -1,18 +1,50 @@
-let div1;
-let leftPos = 0;
+let Module = {}
 
-window.onload = function () {
-   div1 = document.getElementById("div1");
-   setInterval(animate, 10);
-}
+Module.moveBlock = function () {
+   let block = document.getElementById("block");
+   let blockRight = document.getElementById("block-right");
+   let widthScreen = window.screen.width;
+   let leftPos = 0
+   let rightPos = 0;
+   console.log(widthScreen);
 
-function animate() {
-   div1.style.left = leftPos + "px";
-   leftPos += 1;
-   if (leftPos >= window.screenRight) {
-      leftPos -= 1;
-      if (leftPos == 0) {
+   function animateFirst() {
+      if (leftPos <= widthScreen - 30) {
+         block.style.left = leftPos + "px";
          leftPos += 1;
       }
+      // block.classList.add("hidden");
+
+      // function animateSecond() {
+      //    blockRight.classList.add("visible");
+      // }
    }
+
+
+   setInterval(animateFirst, 10);
+
 }
+
+window.addEventListener('load', Module.moveBlock);
+
+
+
+
+// let div1;
+// let leftPos = 0;
+
+// window.onload = function () {
+//    div1 = document.getElementById("div1");
+//    setInterval(animate, 10);
+// }
+
+// function animate() {
+//    div1.style.left = leftPos + "px";
+//    leftPos += 1;
+//    if (leftPos >= window.screenX) {
+//       leftPos -= 1;
+//       if (leftPos == 0) {
+//          leftPos += 1;
+//       }
+//    }
+// }
