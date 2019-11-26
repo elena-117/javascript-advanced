895c72b9b336575fb2963985046a913204f86bf6
elena-117 <3effir@gmail.com>
2019-11-26 16:35:57 +0300

    Дом. задание 4

--- Home_work_4/task_3/js/Module.js
+++ Home_work_4/task_3/js/Module.js
@@ -13,38 +13,10 @@ Module.moveBlock = function () {
          block.style.left = leftPos + "px";
          leftPos += 1;
       }
-      // block.classList.add("hidden");
-
-      // function animateSecond() {
-      //    blockRight.classList.add("visible");
-      // }
    }
-
 
    setInterval(animateFirst, 10);
 
 }
 
 window.addEventListener('load', Module.moveBlock);
-
-
-
-
-// let div1;
-// let leftPos = 0;
-
-// window.onload = function () {
-//    div1 = document.getElementById("div1");
-//    setInterval(animate, 10);
-// }
-
-// function animate() {
-//    div1.style.left = leftPos + "px";
-//    leftPos += 1;
-//    if (leftPos >= window.screenX) {
-//       leftPos -= 1;
-//       if (leftPos == 0) {
-//          leftPos += 1;
-//       }
-//    }
-// }
