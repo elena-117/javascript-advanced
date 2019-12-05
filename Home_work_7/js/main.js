8cf5e467d4c4e48a7d74dda8d478cdd05ea35d6e
elena-117 <3effir@gmail.com>
2019-12-05 07:01:50 +0300

    Дом. задание 7 (пицца)

--- Home_work_7/js/main.js
+++ Home_work_7/js/main.js
@@ -1,4 +1,5 @@
 let Module = {};
+let form = document.formOrder;
 
 function $(id) {
   return document.getElementById(id);
@@ -9,32 +10,26 @@ function $$(cl) {
 }
 
 Module.costCalculation = function() {
-  let form = document.formOrder;
   let totalCostValue = $("totalCostValue");
   let btnSbmit = $("submit-btn-id");
-  let popap = $("popap");
 
   for (let i = 0; i < form.sizesPizza.length; i++) {
-    form.sizesPizza[i].addEventListener("click", costCalculation);
+    form.sizesPizza[i].addEventListener("click", costCalc);
   }
 
-  function costCalculation(e) {
-    document.forms[0].orderTel.style.boxShadow = "none";
-    document.forms[0].orderAddress.style.boxShadow = "none";
-    $("info-1").style.display = "none";
-    $("info-2").style.display = "none";
-    form.orderTel.disabled = false;
-    form.orderAddress.disabled = false;
-    btnSbmit.disabled = false;
+  function costCalc(e) {
     let totalCost = 0;
     let costPizza = e.target.value;
     let costIgredients;
     let images = $$("imgCheckbox");
+    let checkboxInputs = $$("ingredients");
+
+    form.orderTel.disabled = false;
+    form.orderAddress.disabled = false;
+    btnSbmit.disabled = false;
 
     totalCost += parseInt(costPizza);
     totalCostValue.innerHTML = totalCost;
-
-    let checkboxInputs = $$("ingredients");
 
     for (let i = 0; i < checkboxInputs.length; i++) {
       checkboxInputs[i].checked = false;
@@ -50,6 +45,7 @@ Module.costCalculation = function() {
       if (form.elements[i].type == "checkbox") {
         form.elements[i].addEventListener("click", function(e) {
           costIgredients = e.target.value;
+
           if (e.target.checked == true) {
             for (let j = 0; j < images.length; j++) {
               if (images[j].dataset.img == e.target.name) {
@@ -72,66 +68,72 @@ Module.costCalculation = function() {
       }
     }
 
-    btnSbmit.addEventListener("click", function(e) {
-      let pattern = /\d{3}-\d{3}-\d{4}/;
-
-      if (!form.orderTel.value) {
-        form.orderTel.focus();
-        form.orderTel.style.boxShadow = "0 0 1.5px 1px red";
-        form.orderAddress.style.boxShadow = "none";
-        $("info-1").style.display = "block";
-        $("info-2").style.display = "none";
-      }
-      if (!form.orderTel.value.match(pattern) && form.orderTel.value) {
-        form.orderTel.focus();
-        form.orderTel.style.boxShadow = "0 0 1.5px 1px red";
-        // form.orderAddress.style.boxShadow = "none";
-        $("info-1").style.display = "none";
-        $("info-2").style.display = "block";
-      }
-      if (
-        form.orderTel.value.match(pattern) &&
-        form.orderTel.value &&
-        !form.orderAddress.value
-      ) {
-        form.orderAddress.focus();
-        $("info-1").style.display = "block";
-        $("info-2").style.display = "none";
-        form.orderTel.style.boxShadow = "none";
-        form.orderAddress.style.boxShadow = "0 0 1.5px 1px red";
-      }
-      if (form.orderTel.value.match(pattern) && form.orderAddress.value) {
-        document.forms[0].orderTel.style.boxShadow = "none";
-        document.forms[0].orderAddress.style.boxShadow = "none";
-        $("info-1").style.display = "none";
-        $("info-2").style.display = "none";
-        popap.style.display = "block";
-        setTimeout(() => (popap.style.display = "none"), 2000);
-        resetform();
-      }
-    });
+    btnSbmit.addEventListener("click", Module.submitHandler);
   }
-  resetform();
+  Module.resetform();
 };
 
-function resetform() {
+Module.submitHandler = function() {
+  let pattern = /\d{3}-\d{3}-\d{4}/;
+  let popap = $("popap");
+
+  if (!form.orderTel.value) {
+    form.orderTel.focus();
+    form.orderTel.style.boxShadow = "0 0 1.5px 1px red";
+    form.orderAddress.style.boxShadow = "none";
+    $("info-1").style.display = "block";
+    $("info-2").style.display = "none";
+  }
+  if (!form.orderTel.value.match(pattern) && form.orderTel.value) {
+    form.orderTel.focus();
+    form.orderTel.style.boxShadow = "0 0 1.5px 1px red";
+    form.orderAddress.style.boxShadow = "none";
+    $("info-1").style.display = "none";
+    $("info-2").style.display = "block";
+  }
+  if (
+    form.orderTel.value.match(pattern) &&
+    form.orderTel.value &&
+    !form.orderAddress.value
+  ) {
+    form.orderAddress.focus();
+    $("info-1").style.display = "block";
+    $("info-2").style.display = "none";
+    form.orderTel.style.boxShadow = "none";
+    form.orderAddress.style.boxShadow = "0 0 1.5px 1px red";
+  }
+  if (form.orderTel.value.match(pattern) && form.orderAddress.value) {
+    document.forms[0].orderTel.style.boxShadow = "none";
+    document.forms[0].orderAddress.style.boxShadow = "none";
+    $("info-1").style.display = "none";
+    $("info-2").style.display = "none";
+
+    popap.style.display = "block";
+
+    setTimeout(() => (popap.style.display = "none"), 2000);
+
+    Module.resetform();
+  }
+};
+
+Module.resetform = function() {
   $("formOrder").reset();
+
   for (let i = 0; i < $$("ingredients").length; i++) {
     $$("ingredients")[i].setAttribute("disabled", "");
   }
+
   for (let j = 0; j < $$("imgCheckbox").length; j++) {
     $$("imgCheckbox")[j].classList.remove("visible");
     $$("imgCheckbox")[j].classList.add("hidden");
   }
+
   $("totalCostValue").innerHTML = 0;
 
   document.forms[0].orderTel.disabled = true;
   document.forms[0].orderAddress.disabled = true;
-  document.forms[0].orderTel.style.boxShadow = "none";
-  document.forms[0].orderAddress.style.boxShadow = "none";
-  $("info-1").style.display = "none";
-  $("info-2").style.display = "none";
+
   $("submit-btn-id").disabled = true;
-}
+};
 
 window.addEventListener("load", Module.costCalculation);
